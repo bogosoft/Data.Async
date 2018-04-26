baf04ffcc8011bc6867ea60558776b268aecd9ed
Clayton Roth <croth0627@yahoo.com>
2018-04-26 17:59:36 -0600

    removed unnecessary preprocessor instruction

--- Bogosoft.Data.Async.Tests/End2EndTests.cs
+++ Bogosoft.Data.Async.Tests/End2EndTests.cs
@@ -44,11 +44,7 @@ namespace Bogosoft.Data.Async.Tests
                 {
                     DistanceToPrimary = reader.GetFieldValue<float>(PrimaryDistance)
                 },
-#if NET45
                 Type = (CelestialBodyType)Enum.Parse(typeof(CelestialBodyType), reader.GetFieldValue<string>(Type))
-#else
-                Type = Enum.Parse<CelestialBodyType>(reader.GetFieldValue<string>(Type))
-#endif
             };
         }
 
