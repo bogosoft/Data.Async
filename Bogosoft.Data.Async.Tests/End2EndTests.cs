638ba367a8bb5133c08601954dfece1040879a6a
Clayton Roth <croth0627@yahoo.com>
2018-04-26 18:08:29 -0600

    removed helper method

--- Bogosoft.Data.Async.Tests/End2EndTests.cs
+++ Bogosoft.Data.Async.Tests/End2EndTests.cs
@@ -19,21 +19,6 @@ namespace Bogosoft.Data.Async.Tests
 
         static List<FieldAdapter<CelestialBody>> Columns = new List<FieldAdapter<CelestialBody>>();
 
-        static bool AreEqual(CelestialBody a, CelestialBody b)
-        {
-            if (a is null && b is null)
-            {
-                return true;
-            }
-            else
-            {
-                return a.Mass == b.Mass
-                    && a.Name == b.Name
-                    && a.Orbit?.DistanceToPrimary == b.Orbit?.DistanceToPrimary
-                    && a.Type == b.Type;
-            }
-        }
-
         static CelestialBody ReadCelestialBody(DbDataReader reader)
         {
             return new CelestialBody
@@ -71,7 +56,22 @@ namespace Bogosoft.Data.Async.Tests
                 {
                     (await e.MoveNextAsync()).ShouldBeTrue();
 
-                    AreEqual(a.Current, e.Current).ShouldBeTrue();
+                    a.Current.ShouldNotBeNull();
+                    e.Current.ShouldNotBeNull();
+
+                    a.Current.Mass.ShouldBe(e.Current.Mass);
+                    a.Current.Name.ShouldBe(e.Current.Name);
+
+                    if (a.Current.Orbit is null)
+                    {
+                        e.Current.Orbit.ShouldBeNull();
+                    }
+                    else
+                    {
+                        a.Current.Orbit.DistanceToPrimary.ShouldBe(e.Current.Orbit.DistanceToPrimary);
+                    }
+
+                    a.Current.Type.ShouldBe(e.Current.Type);
                 }
 
                 (await e.MoveNextAsync()).ShouldBeFalse();
